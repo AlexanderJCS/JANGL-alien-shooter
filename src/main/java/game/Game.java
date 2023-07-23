c2e1610810c385e5f5b0971e26a6a281aa788794
Alex <alexandercastronovo@gmail.com>
2023-07-23 14:05:15 -0400

    Add pausing

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -7,13 +7,19 @@ import jangl.graphics.Camera;
 import jangl.graphics.font.Font;
 import jangl.graphics.font.Text;
 import jangl.io.Window;
+import jangl.io.keyboard.KeyEvent;
+import jangl.io.keyboard.Keyboard;
 import jangl.time.Clock;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
 
 public class Game implements AutoCloseable {
     private final Player player;
     private final EnemySpawner enemySpawner;
     private final GameMap gameMap;
     private final Text enemiesCounter;
+    private boolean paused;
 
     public Game() {
         this.gameMap = new GameMap();
@@ -24,6 +30,7 @@ public class Game implements AutoCloseable {
         Font arial = new Font("src/main/resources/font/arial.fnt", "src/main/resources/font/arial.png");
         arial.setObeyCamera(false);
         this.enemiesCounter = new Text(new WorldCoords(0.05f, 0.95f), arial, 0.05f, "");
+        this.paused = false;
     }
 
     public void update() {
@@ -50,10 +57,23 @@ public class Game implements AutoCloseable {
         this.enemiesCounter.draw();
     }
 
+    public void pauseCheck(List<KeyEvent> keyEvents) {
+        for (KeyEvent event : keyEvents) {
+            if (event.action == GLFW.GLFW_PRESS && event.key == GLFW.GLFW_KEY_P) {
+                this.paused = !this.paused;
+                this.enemiesCounter.setText(this.enemiesCounter.getText() + " | PAUSED");
+            }
+        }
+    }
+
     public void run() {
         while (Window.shouldRun()) {
-            this.update();
+            this.pauseCheck(Keyboard.getEvents());
             this.draw();
+
+            if (!this.paused) {
+                this.update();
+            }
 
             Window.setTitle("Alien Shooter | FPS: " + Math.round(Clock.getSmoothedFps()));
 
