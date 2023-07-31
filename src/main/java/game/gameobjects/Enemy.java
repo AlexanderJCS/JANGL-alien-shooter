17eac2a9a569fbc9a35d3c8c0cdc152cb77f8389
Alex <alexandercastronovo@gmail.com>
2023-07-30 23:28:35 -0400

    Reduce enemy spread even more

--- src/main/java/game/gameobjects/Enemy.java
+++ src/main/java/game/gameobjects/Enemy.java
@@ -47,7 +47,7 @@ public class Enemy extends GameObject implements Destroyable {
         this.animationSwitch = new Cooldown(0.05f);
         this.animationState = true;
 
-        this.angleOffset = Consts.RANDOM.nextFloat((float) -Math.PI / 9, (float) Math.PI / 9);
+        this.angleOffset = Consts.RANDOM.nextFloat((float) -Math.PI / 18, (float) Math.PI / 18);
     }
 
     @Override
